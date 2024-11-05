9bb6acb8f1c56a79de80d59260ec31963ca506e7
BINAYA THAPA MAGAR <binayathapamagar@BINAYAs-MacBook-Pro.local>
2024-11-04 23:31:16 -0500

    Added loading spinner when fetching the replies

--- Threads/Core/ThreadDetails/View/ThreadDetailsView.swift
+++ Threads/Core/ThreadDetails/View/ThreadDetailsView.swift
@@ -20,73 +20,84 @@ struct ThreadDetailsView: View {
     }
     
     var body: some View {
-        ScrollView {
-            VStack {
-                HStack {
-                    CircularProfileImageView(user: thread.user, size: .small)
+        ZStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        CircularProfileImageView(user: thread.user, size: .small)
+                        
+                        Text(thread.user?.username ?? "@username")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                        
+                        Text(thread.timestamp.timeAgoDisplay())
+                            .font(.footnote)
+                            .foregroundStyle(Color(.systemGray3))
+                        
+                        Spacer()
+                        
+                        Button(action: {}, label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundStyle(Color(.darkGray))
+                        })
+                    }//HStack
+                    .padding(.horizontal)
                     
-                    Text(thread.user?.username ?? "@username")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                    
-                    Text(thread.timestamp.timeAgoDisplay())
-                        .font(.footnote)
-                        .foregroundStyle(Color(.systemGray3))
-                    
-                    Spacer()
-                    
-                    Button(action: {}, label: {
-                        Image(systemName: "ellipsis")
-                            .foregroundStyle(Color(.darkGray))
-                    })
-                }//HStack
-                .padding(.horizontal)
-                
-                VStack(alignment: .leading, spacing: 12) {
-                    Text(thread.content)
-                        .font(.subheadline)
+                    VStack(alignment: .leading, spacing: 12) {
+                        Text(thread.content)
+                            .font(.subheadline)
 
-                    ContentActionButtonsView(thread: thread)
+                        ContentActionButtonsView(thread: thread)
+                    }//VStack
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    Divider()
+                        .padding(.top)
+                    
+                    HStack {
+                        Text("Replies")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Button {
+                            print("Show user likes")
+                        } label: {
+                            HStack(spacing: 4) {
+                                Text("View activity")
+                                
+                                Image(systemName: "chevron.right")
+                            }//HStack
+                            .font(.subheadline)
+                            .foregroundStyle(.dividerBG)
+                        }//Button
+                    }//HStack
+                    .padding(.vertical, 8)
+                    .padding(.horizontal)
+                    
+                    Divider()
+                        .padding(.bottom)
+                    
+                    LazyVStack{
+                        ForEach(viewModel.replies) { reply in
+                            ThreadReplyCellView(threadReply: reply)
+                        }
+                    }//LazyVStack
                 }//VStack
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
+            }//ScrollView
+            .padding(.vertical)
+            
+            if viewModel.showLoadingSpinner {
+                Color.black.opacity(0.4)
+                    .edgesIgnoringSafeArea(.all)
                 
-                Divider()
-                    .padding(.top)
-                
-                HStack {
-                    Text("Replies")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                    
-                    Spacer()
-                    
-                    Button {
-                        print("Show user likes")
-                    } label: {
-                        HStack(spacing: 4) {
-                            Text("View activity")
-                            
-                            Image(systemName: "chevron.right")
-                        }//HStack
-                        .font(.subheadline)
-                        .foregroundStyle(.dividerBG)
-                    }//Button
-                }//HStack
-                .padding(.vertical, 8)
-                .padding(.horizontal)
-                
-                Divider()
-                    .padding(.bottom)
-                
-                LazyVStack{
-                    ForEach(viewModel.replies) { reply in
-                        ThreadReplyCellView(threadReply: reply)
-                    }
-                }//LazyVStack
-            }//VStack
-        }//ScrollView
-        .padding(.vertical)
+                VStack {
+                    AuthProgressView()
+                }
+            }
+        }
         .navigationTitle("Thread")
         .navigationBarTitleDisplayMode(.inline)
     }
